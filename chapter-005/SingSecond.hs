362c5c8a2ba5112a905b5e8183f38ad422a21415
gabriele.lana <gabriele.lana@cleancode.it>
2018-06-08 10:01:42 +0200

    Chapter 5

---
+++ chapter-005/SingSecond.hs
@@ -0,0 +1,13 @@
+
+module SingSecond where
+
+fstString :: [Char] -> [Char]
+fstString x = x ++ " in the rain"
+
+sndString :: [Char] -> [Char]
+sndString x = x ++ " over the rainbow"
+
+sing = if (x > y) then fstString x else sndString y
+  where
+    x = "Singin"
+    y = "Somewhere"
