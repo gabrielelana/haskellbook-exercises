09add848cb6d3342b6aac63834153dc6399cce53
gabriele.lana <gabriele.lana@cleancode.it>
2018-07-01 15:58:45 +0200

    Chapter 9

---
+++ chapter-009/Cipher.hs
@@ -0,0 +1,23 @@
+
+module Cipher where
+
+import Data.Char
+
+encode :: Int -> String -> String
+encode n s = map (encodeChar n) s
+
+decode :: Int -> String -> String
+decode n s = map (decodeChar n) s
+
+decodeChar :: Int -> Char -> Char
+decodeChar n = encodeChar (-n)
+
+encodeChar :: Int -> Char -> Char
+encodeChar n c
+  | elem c ['a'..'z'] = shift n c
+  | otherwise = c
+
+shift :: Int -> Char -> Char
+shift n c = chr ((mod ((ord c - base) + n) l) + base)
+  where l = (ord 'z') - (ord 'a') + 1
+        base = ord 'a'
---
+++ chapter-009/PoemLines.hs
@@ -0,0 +1,34 @@
+
+module PoemLines where
+
+firstSen = "Tyger Tyger, burning bright\n"
+secondSen = "In the forests of the night\n"
+thirdSen = "What immortal hand or eye\n"
+fourthSen = "Could frame thy fearful symmetry?"
+
+sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
+-- putStrLn sentences -- should print
+-- Tyger Tyger, burning bright
+-- In the forests of the night
+-- What immortal hand or eye
+-- Could frame thy fearful symmetry?
+
+-- Implement this
+lines :: String -> [String]
+lines "" = []
+lines s = (takeWhile (/= '\n') s) : lines ((dropWhile (== '\n') . dropWhile (/= '\n')) s)
+
+-- What we want 'lines sentences'
+-- to equal
+shouldEqual =
+  [ "Tyger Tyger, burning bright"
+  , "In the forests of the night"
+  , "What immortal hand or eye"
+  , "Could frame thy fearful symmetry?"
+  ]
+
+-- The main function here is a small test -- to ensure you've written your function -- correctly.
+main :: IO ()
+main = print $
+       "Are they equal? "
+       ++ show (lines sentences == shouldEqual)
