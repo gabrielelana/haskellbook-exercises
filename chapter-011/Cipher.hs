c6fa8f21262af6c6e1ebc63baafb3ac19dca8895
gabriele.lana <gabriele.lana@cleancode.it>
2018-08-26 17:08:57 +0200

    Implement back vigenereDecode and caesarDecode

--- chapter-011/Cipher.hs
+++ chapter-011/Cipher.hs
@@ -9,8 +9,15 @@ vigenereEncode :: Alphabet -> String -> String -> String
 vigenereEncode a keyword message = map (vigenereEncodeChar a) (zip message keywordForMessage)
   where keywordForMessage = replaceWithKeyword a keyword message
 
+vigenereDecode :: Alphabet -> String -> String -> String
+vigenereDecode a keyword message = map (vigenereDecodeChar a) (zip message keywordForMessage)
+  where keywordForMessage = replaceWithKeyword a keyword message
+
 vigenereEncodeChar :: Alphabet -> (Char, Char) -> Char
 vigenereEncodeChar a (mc, kc) = caesarEncodeChar a ((ord kc) - (ord $ head a)) mc
+
+vigenereDecodeChar :: Alphabet -> (Char, Char) -> Char
+vigenereDecodeChar a (mc, kc) = caesarDecodeChar a ((ord kc) - (ord $ head a)) mc
 
 replaceWithKeyword :: Alphabet -> String -> String -> String
 replaceWithKeyword a keyword message = replace a (cycle keyword) message
@@ -22,10 +29,16 @@ replaceWithKeyword a keyword message = replace a (cycle keyword) message
 caesarEncode :: Alphabet -> Int -> String -> String
 caesarEncode a n s = map (caesarEncodeChar a n) s
 
+caesarDecode :: Alphabet -> Int -> String -> String
+caesarDecode a n s = map (caesarDecodeChar a n) s
+
 caesarEncodeChar :: Alphabet -> Int -> Char -> Char
 caesarEncodeChar a n c
   | canBeEncoded a c = shift a n c
   | otherwise = c
+
+caesarDecodeChar :: Alphabet -> Int -> Char -> Char
+caesarDecodeChar a n c = caesarDecodeChar a (-n) c
 
 canBeEncoded :: Alphabet -> Char -> Bool
 canBeEncoded a c = elem c a
