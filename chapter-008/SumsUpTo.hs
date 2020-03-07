0e0082e5d5abb654f45fec5f4cd84dcc35133589
gabriele.lana <gabriele.lana@gmail.com>
2020-03-07 16:18:22 +0100

    Review Chapter 08

--- chapter-008/SumsUpTo.hs
+++ chapter-008/SumsUpTo.hs
@@ -4,3 +4,9 @@ sumsUpTo :: (Eq a, Num a) => a -> a
 sumsUpTo 0 = 0
 sumsUpTo 1 = 1
 sumsUpTo n = n + sumsUpTo (n - 1)
+
+sumsUpTo' :: (Eq a, Num a) => a -> a
+sumsUpTo' n = up 1 n
+  where up m n
+          | m == n    = n
+          | otherwise = m + (up (m + 1) n)
